7f7b8bbebc5535240e19900f74e85ea7890e47bd
Alex Meleshko <meleshko.a@light-it.net>
2024-04-01 12:21:53 +0300

    Add Intro screen

---
+++ app/src/main/java/com/strategic/grid/app/IntroActivity.kt
@@ -0,0 +1,34 @@
+package com.strategic.grid.app
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = ContextCompat.getColor(context, R.color.background)
+            navigationBarColor = ContextCompat.getColor(context, R.color.background)
+        }
+
+        val nextBtn = findViewById<TextView>(R.id.tv_next)
+        nextBtn.setOnClickListener {
+            val preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+            val editor = preferences.edit()
+            editor.putBoolean("isFirstRun", false)
+            editor.apply()
+
+            var intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/strategic/grid/app/RoutingActivity.kt
@@ -0,0 +1,24 @@
+package com.strategic.grid.app
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoutingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = if (isFirstRun()) Intent(this, IntroActivity::class.java)
+        else Intent(this, MainActivity::class.java)
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    private fun isFirstRun(): Boolean {
+        val preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        return preferences.getBoolean("isFirstRun", true)
+    }
+}
